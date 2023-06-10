15bddc942357eb5b6138bdb2e9e2783341885d56
CyanUnderscore <106006219+CyanUnderscore@users.noreply.github.com>
2023-06-10 13:36:01 +0200

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::fs::File;
-use std::cell::RefCell;
-use std::rc::Rc;
 use std::io::Write;
 use std::path::Path;
 use curl::{easy::Easy};
-use std::cmp::PartialEq;
 use eframe::egui;
-use eframe::Result as eResult;
 use curl_sys;
+use egui::{Color32, TextStyle};
+use std::panic;
+use std::error::Error;
 
-pub type Result<T, E = Error> = std::result::Result<T, E>;
+pub type Result<T, E = dyn Error> = std::result::Result<T, E>;
 
-
+/*
 pub struct Error{
     pub code: curl_sys::CURLcode,
     pub extra: Option<Box<str>>,
-}
-fn main() {
+}*/
+
+fn main() -> Result<(), eframe::Error>  {
+
+    //defining the color used
+
+
     //env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
     let options = eframe::NativeOptions {
@@ -30,9 +34,12 @@ fn main() {
     let mut name = "exemple.png".to_owned();
     let mut path = "/path/to/storage".to_owned();
     let mut url = "https://site.com/image.png".to_owned();
+    let mut resultat = String::new();
+    let mut result_label_color = Color32::from_rgb(255, 255, 255);
 
     eframe::run_simple_native("Curl 0.0.2", options, move |ctx, _frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
+
             ui.heading("The download service of your dreams");
             ui.horizontal(|ui| {
                 let name_label = ui.label("file name :");
@@ -49,17 +56,33 @@ fn main() {
                 ui.text_edit_singleline(&mut url)
                     .labelled_by(url_label.id);
             });
-            if ui.button("Click each year").clicked() {
-                match download(name.clone(), url.clone(), path.clone()) {
-                    Ok(()) => println!("succes"),
-                    Err(Error{code, extra}) => println!("{:?}", code),
+
+            
+            ui.colored_label(result_label_color ,resultat.clone());
+
+            if ui.button("Download").clicked() {
+                let result = panic::catch_unwind(||{
+                    download(name.clone(), url.clone(), path.clone());
+                });
+                match result {
+                    Ok(()) => {
+                        resultat = "success".to_owned();
+                        result_label_color = Color32::from_rgb(0, 255, 0)},
+                    Err(error) => {
+                        result_label_color = Color32::from_rgb(255, 0, 0);
+                        if let Some(err) = error.downcast_ref::<Box<dyn Error>>() {
+                            resultat = err.as_ref().to_string();
+                        } else {
+                            resultat = "the url or the path might be unusable".to_owned();
+                        }
+                    }
                 }
             }
         });
-    });
+    })
 }
 
-fn download(name: String, url: String, path: String) -> Result<(), Error> {
+fn download(name: String, url: String, path: String) {
     let wanted_name = name;
     let wanted_file = url;
     let binding = &(path.trim().to_owned() + wanted_name.trim());
@@ -69,7 +92,7 @@ fn download(name: String, url: String, path: String) -> Result<(), Error> {
 
     let mut easy = Easy::new();
     println!("Fetching from: {}", &wanted_file);
-    easy.url(&wanted_file.trim());
+    easy.url(&wanted_file.trim()).unwrap();
     easy.follow_location(true).unwrap();
 
     easy.write_function(move |data| {
@@ -78,5 +101,4 @@ fn download(name: String, url: String, path: String) -> Result<(), Error> {
     }).unwrap();
 
     easy.perform().unwrap();
-    Ok(())
 }
